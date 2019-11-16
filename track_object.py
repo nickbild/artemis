9d11a23571a69bcd5d8eab6fdda443ccc9987ed2
Nick Bild <nick.bild@gmail.com>
2019-11-16 15:50:41 -0500

    Save history of object detection for smoothing.

--- track_object.py
+++ track_object.py
@@ -16,6 +16,11 @@ cap.set(4, 720)
 # Initialize DL model.
 ssd_model, utils, classes_to_labels = infer.init_model()
 
+loop = 0
+obj = []
+for i in range(15):
+    obj.append({'x': None, 'y': None, 'w': None, 'h': None})
+
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, (300, 300))
@@ -23,14 +28,35 @@ while True:
     # Locate the object of interest in the current frame.
     obj_x, obj_y, obj_w, obj_h = infer.locate_object(frame, obj_of_interest, ssd_model, utils, classes_to_labels)
 
+    
+    obj[loop] = {'x': obj_x, 'y': obj_y, 'w': obj_w, 'h': obj_h}
+
+    loop += 1
+
+    if loop == 15:
+        loop = 0
+
+    found = False
+    found_at = 0
+    for i in range(14, -1, -1):
+        if obj[i]['x'] is not None:
+            found = True
+            found_at = i
+
     # If the object was found...
-    if obj_x is not None:
+    if found:
+        obj_x = obj[found_at]['x']
+        obj_y = obj[found_at]['y']
+        obj_w = obj[found_at]['w']
+        obj_h = obj[found_at]['h']
+
         # Turn laser on.
         if not laser_on:
             laser_on = True
             laser_control.on()
 
         laser_coords = laser_tracker.detect(frame)
+        print(laser_coords)
 
         if laser_coords is not None:
             laser_x = laser_coords[0]
